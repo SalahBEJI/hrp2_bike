0fa117c09f36740e5809dfb7f54a003aaa33618f
Benoît Sinivassin <benoit.sinivassin@laas.fr>
2015-06-18 22:48:08 +0200

    2nd commit

---
+++ build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/bsinivas/devel/package/hrp2_bike/lib/libgtest.pdb"
+  "/home/bsinivas/devel/package/hrp2_bike/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ scripts/appli.py
@@ -0,0 +1,3 @@
+from dynamic_graph.sot.application.velocity.precomputed_tasks import Application
+appli =Application(robot)
+robot.initializeTracer()
