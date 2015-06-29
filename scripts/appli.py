fa98ebf6d60972d8cd3250e8d521d59b58f84f4e
Benoît Sinivassin <benoit.sinivassin@laas.fr>
2015-06-29 17:02:20 +0200

    creation task BikeSitting

--- scripts/appli.py
+++ scripts/appli.py
@@ -1,28 +1,20 @@
 from dynamic_graph.sot.application.cycling import Hrp2Bike, Sequencer
+from dynamic_graph.script_shortcuts import optionalparentheses
 
 hrp2Bike = Hrp2Bike(robot)
 sequencer = Sequencer(hrp2Bike)
 
-robot.initializeTracer()
 # --- SHORTCUTS
 
 push              = hrp2Bike.push
-taskHalfSitting   = hrp2Bike.taskHalfSitting
 sot               = hrp2Bike.sot
 taskBikeSitting   = hrp2Bike.taskBikeSitting
+taskHalfSitting   = hrp2Bike.taskHalfSitting
+removeTasks       = hrp2Bike.removeTasks
 
+gopen        = optionalparentheses(appli.openGripper)
+gclose       = optionalparentheses(appli.closeGripper)
+gohs         = optionalparentheses(appli.goHalfSitting)
+gobs         = optionalparentheses(appli.goBikeSitting)
 
 s = sequencer
-
-
-
-
-
-
-
-
-
-
-
-
-
--- scripts/cycling.py
+++ scripts/cycling.py
@@ -1,7 +1,7 @@
 from dynamic_graph.sot.core.meta_tasks import generic6dReference, Task, GainAdaptive
 from dynamic_graph.sot.core.matrix_util import matrixToTuple
 from dynamic_graph.sot.core.meta_tasks import *
-from dynamic_graph.sot.core.meta_task_posture import MetaTaskPosture
+from dynamic_graph.sot.core.meta_task_posture import MetaTaskPosture, MetaTaskKinePosture
 from dynamic_graph import plug
 from numpy import *
 from dynamic_graph.sot.application.velocity.precomputed_tasks import Application
@@ -11,13 +11,29 @@ toList = lambda sot: map(lambda x: x[2:],sot.display().split('\n')[3:-2])
 
 class Hrp2Bike(Application):
 
+    bikeSitting = (
+        # Free flyer
+        0., 0., 0.648702, 0., 0. , 0.,
+
+        # Legs
+        0., 0., -0.453786, 0.872665, -0.418879, 0.,
+        0., 0., -0.453786, 0.872665, -0.418879, 0.,
+
+        # Chest and head
+        0., 0., 0., 0.,
+
+        # Arms
+        0.261799, -0.17453, 0., -0.523599, 0., 0., 0.1,
+        0.261799, 0.17453,  0., -0.523599, 0., 0., 0.1,
+        )
+
     def __init__(self,robot):
         Application.__init__(self,robot)
 
         self.sot=self.solver.sot
         self.robot=robot
-
-        self.initTasks(robot)
+        self.createTasks(robot)
+        self.initTasks()
         self.initTaskGains()
         self.initialStack()
 
@@ -26,10 +42,7 @@ class Hrp2Bike(Application):
 
 
     #----------TASKS-------------------------------
-    def initTasks(self,robot):
-        self.initTaskBalance()
-        self.createBikeSittingTask(robot, 'bike-sitting')
-        self.createTasks(robot)
+    #------------------CREATION--------------------
 
     def createTrunkTask (self, robot, taskName, ingain=1.0):
         task = Task (taskName)
@@ -42,18 +55,34 @@ class Hrp2Bike(Application):
         plug(task.error, gain.error)
         return (task, gain)
 
-    def createBikeSittingTask (self, robot, taskName, ingain=1.0):
-        task = Task (taskName)
-        task.add (self.features['left-ankle'].name)
-        task.add (self.features['right-ankle'].name)
-        task.add (self.features['com'].name)
-        task.add (self.features['right-wrist'].name)
-        task.add (self.features['left-wrist'].name)
-        gain = GainAdaptive(taskName+'gain')
-        gain.setConstant(ingain)
-        plug(gain.gain, task.controlGain)
-        plug(task.error, gain.error)
-        return (task, gain)
+    def createTasks(self,robot):
+        self.taskHalfSitting = MetaTaskKinePosture(self.robot.dynamic,'halfsitting')
+        self.taskBikeSitting = MetaTaskKinePosture(self.robot.dynamic,'bike-sitting')
+        self.taskGripper     = MetaTaskKinePosture(self.robot.dynamic,'gripper')
+
+        (self.tasks['trunk'],self.gains['trunk'])= self.createTrunkTask(robot, 'trunk')
+
+        self.taskRF          = self.tasks['left-ankle']
+        self.taskLF          = self.tasks['right-ankle']
+        self.taskCom         = self.tasks['com']
+        self.taskRH          = self.tasks['right-wrist']
+        self.taskLH          = self.tasks['left-wrist']
+        self.taskTrunk       = self.tasks['trunk']
+        self.taskPosture     = self.tasks['posture']
+        self.taskBalance     = self.tasks['balance']
+
+    def openGripper(self):
+        self.taskGripper.gotoq(None,rhand=(self.gripperOpen,),lhand=(self.gripperOpen,))
+
+    def closeGripper(self):
+        self.taskGripper.gotoq(None,rhand=(self.gripperClose,),lhand=(self.gripperClose,))
+
+    #------------------INIT-TASK------------------
+    def initTasks(self):
+        self.initTaskBalance()
+        self.initTaskGripper()
+        self.initTaskHalfSitting()
+        self.initTaskBikeSitting()
 
     def initTaskBalance(self):
         # --- BALANCE ---
@@ -65,24 +94,32 @@ class Hrp2Bike(Application):
         self.features['gaze'].selec.value = '111000'
         self.featureCom.selec.value = '111'
 
-    def createTasks(self,robot):
-        (self.tasks['trunk'],self.gains['trunk'])= self.createTrunkTask(robot, 'trunk')
-        (self.tasks['bike-sitting'],self.gains['bike-sitting'])= self.createBikeSittingTask(robot, 'bike-sitting')
-        self.taskRF      = self.tasks['left-ankle']
-        self.taskLF      = self.tasks['right-ankle']
-        self.taskCom     = self.tasks['com']
-        self.taskRH      = self.tasks['right-wrist']
-        self.taskLH      = self.tasks['left-wrist']
-        self.taskTrunk   = self.tasks['trunk']
-        self.taskHalfSitting = MetaTaskPosture(self.robot.dynamic,'halfsitting')
-        self.taskPosture = self.tasks['posture']
-        self.taskBalance = self.tasks['balance']
-        self.taskBikeSitting = self.tasks['bike-sitting']
+    def initTaskHalfSitting(self):
+        self.taskHalfSitting.gotoq(None,\
+                                    rleg =(self.robot.halfSitting[6:12]),   \
+                                    lleg =(self.robot.halfSitting[12:18]),  \
+                                    chest=(self.robot.halfSitting[18:20]),  \
+                                    head =(self.robot.halfSitting[20:22]),  \
+                                    rarm =(self.robot.halfSitting[22:28]),  \
+                                    larm =(self.robot.halfSitting[29:35]))
 
+    def initTaskBikeSitting(self):
+        self.taskBikeSitting.gotoq(None,\
+                                    rleg =(self.bikeSitting[6:12]),   \
+                                    lleg =(self.bikeSitting[12:18]),  \
+                                    chest=(self.bikeSitting[18:20]),  \
+                                    head =(self.bikeSitting[20:22]),  \
+                                    rarm =(self.bikeSitting[22:28]),  \
+                                    larm =(self.bikeSitting[29:35]))
 
+    def initTaskGripper(self):
+        self.gripperOpen = 0.6
+        self.gripperClose = 0.05
+        self.closeGripper()
+
+    #------------------INIT-GAIN------------------
     def initTaskGains(self):
         self.taskHalfSitting.gain.setByPoint(2,0.2,0.01,0.8)
-
 
     #----------SOLVER------------------------------
 
@@ -99,9 +136,10 @@ class Hrp2Bike(Application):
             if isinstance(task,str): taskName=task
             elif "task" in task.__dict__:  taskName=task.task.name
             else: taskName=task.name
-            self.sot.remove(taskName)
+            if taskName in toList(self.sot):
+                self.sot.remove(taskName)
 
-    def printSolver(self): #show tasks in solver controling the robot
+    def printSolver(self): #show tasks in solver controlling the robot
         print self.solver
 
     def showTasks(self) : #show library of precomputed tasks
@@ -117,13 +155,15 @@ class Hrp2Bike(Application):
         self.push(self.taskPosture)
 
     def goHalfSitting(self):
-        self.featurePostureDes.errorIN.value = self.robot.halfSitting
         self.sot.clear()
         self.push(self.taskBalance)
         self.push(self.taskPosture)
+        self.push(self.taskHalfSitting)
 
-    def bikeSitting(self):
+
+    def goBikeSitting(self):
         self.sot.clear()
+        self.push(self.taskTrunk)
         self.push(self.taskBikeSitting)
 
 
@@ -139,7 +179,8 @@ class Sequencer:
             self.step+=1
             print self.step
         if self.step==1:
-            self.hrp2Bike.bikeSitting
+
+            self.hrp2Bike.goBikeSitting
             self.step+=1
             print self.step
         else:
